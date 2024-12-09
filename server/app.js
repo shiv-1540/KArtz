6ca1eeeb278a91d98d387bf5bac18ee4b58cc6a0
shivshankar <shivghyar538@gmail.com>
2024-12-09 22:12:53 +0530

    Added vercel.json to server

--- server/app.js
+++ server/app.js
@@ -33,6 +33,9 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+app.get("/", (req, res) => {
+    res.json("Hello");
+})
 
 
 const transporter = nodemailer.createTransport({
